593b055e0ad506d70b85fce4f9757556888231ef
gitteroy <elroyliangbojun@gmail.com>
2025-08-25 13:54:57 +0800

    feat: terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,8 @@
-# Specify the required AWS provider version
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.2.0"
     }
   }
 }
@@ -16,7 +15,6 @@ resource "aws_s3_bucket" "website" {
   bucket = var.s3_bucket_name
 }
 
-# Configure the S3 bucket for static website hosting
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.website.id
 
@@ -25,8 +23,6 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
 }
 
-# Unblock public access to the bucket.
-# This is required to allow a public bucket policy.
 resource "aws_s3_bucket_public_access_block" "website" {
   bucket = aws_s3_bucket.website.id
 
@@ -36,28 +32,24 @@ resource "aws_s3_bucket_public_access_block" "website" {
   restrict_public_buckets = false
 }
 
-# A data source to create the public read policy document
 data "aws_iam_policy_document" "public_read" {
   statement {
     principals {
       type        = "AWS"
-      identifiers = ["*"] # Represents everyone/anonymous
+      identifiers = ["*"]
     }
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.website.arn}/*"] # Policy applies to all objects in the bucket
+    resources = ["${aws_s3_bucket.website.arn}/*"]
   }
 }
 
-# Apply the public read policy to the bucket
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
   policy = data.aws_iam_policy_document.public_read.json
 
-  # This ensures the public access block is configured before the policy is applied
   depends_on = [aws_s3_bucket_public_access_block.website]
 }
 
-# Output the website URL so you can easily access it
 output "website_endpoint" {
   description = "The public URL for the S3 website."
   value       = aws_s3_bucket_website_configuration.website.website_endpoint
