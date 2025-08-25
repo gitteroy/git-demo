6f6579961ad23c6829f40a5481beee1db8f565ac
gitteroy <elroyliangbojun@gmail.com>
2025-08-25 16:10:15 +0800

    feat: apigateway logs

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -26,3 +26,21 @@ resource "aws_lambda_permission" "api_gw" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.this.execution_arn}/*"
 }
+
+# This resource creates a CloudWatch log group for our API logs
+resource "aws_cloudwatch_log_group" "api_logs" {
+  name              = "/aws/api_gateway/${var.api_name}"
+  retention_in_days = 7
+}
+
+# This explicitly defines the default stage and enables access logging
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = "$default"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_logs.arn
+    format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId"
+  }
+}
